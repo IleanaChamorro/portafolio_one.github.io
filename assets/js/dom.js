805a32766f77fdc3ed754b6b9429ffaacb198f93
ileanachamorro <ic.chamorro07@gmail.com>
2022-12-20 02:00:06 -0300

    initial commit

---
+++ assets/js/dom.js
@@ -0,0 +1,8 @@
+import scrollTopButton from "./script.js";
+import hamburgerMenu from "./menuHamburger.js"
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    scrollTopButton(".scroll-top-btn");
+    hamburgerMenu
+});
